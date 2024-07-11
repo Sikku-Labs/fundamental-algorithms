ce2f2776d6160e72f5ed645111fa43b1a4d277d6
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 15:53:30 +0530

    Update print

---
+++ main/core-python/print/mainP1.py
@@ -0,0 +1,13 @@
+"""
+Main File Print 1
+sidb95
+bhatoresiddharth@gmail.com 
+11 July 2024
+"""
+
+from print import Print
+
+class Solution:
+  def mainP1(self, s: str) -> None:
+    P1 = Print()
+    P1.printString(s)
