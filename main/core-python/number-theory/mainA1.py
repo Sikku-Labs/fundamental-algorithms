6a028a14efe48591eee4c2167b75e346b4ca0e27
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 16:12:46 +0530

    Update number-theory

---
+++ main/core-python/number-theory/mainA1.py
@@ -0,0 +1,16 @@
+"""
+mainA1.py
+sidb95
+bhatoresiddharth@gmail.com 
+11 July 2024
+"""
+
+from add import Add
+
+class Solution:
+  def __init__(self):
+    pass
+
+  def mainA1(self, a: int, b: int) -> int:
+    A1 = Add()
+    return A1.sumTwoNums(a, b)
