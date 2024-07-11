4311d20b61ea142e936854e8e0437875d2c93929
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 15:23:47 +0530

    Update print

---
+++ main/print/print.h
@@ -0,0 +1,14 @@
+/*
+print.h
+sidb95
+bhatoresiddharth@gmail.com 
+11 July 2024
+*/
+
+class Print {
+public:
+    int printString(string strA) {
+        cout << strA << endl;
+        return;
+    }
+};
