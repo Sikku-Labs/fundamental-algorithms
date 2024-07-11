379728185a07b07d7ca17ccface11ce299148bd3
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-11 15:13:45 +0530

    Update number-theory

---
+++ main/number-theory/mainSum1.h
@@ -0,0 +1,16 @@
+/*
+Main File Sum 1
+sidb95
+bhatoresiddharth@gmail.com 
+11 July 2024
+*/
+
+class Solution {
+public:
+    int sumTwoNums(int a, int b) {
+        for (int i = 0; i < b; i += 1) {
+            a += 1;
+        }
+        return a;
+    }
+};
