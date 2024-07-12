4fd5e42e8b819db98d957c05e770d45ff3e5dd50
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-12 22:37:48 +0530

    Update bubble_sort.h

--- main/core-C/sorting/bubble_sort.h
+++ main/core-C/sorting/bubble_sort.h
@@ -4,3 +4,20 @@ sidb95
 bhatoresiddharth@gmail.com 
 12 July 2024
 */
+
+int* bubble_sort(int* nums, int n) {
+    for (int i = 0; i < n - 1; i += 1) {
+        int FLAG = true;
+        for (int j = i + 1; j < n; j += 1) {
+            if (nums[j] < nums[i]) {
+                swap(nums, i, j);
+                FLAG = false;
+                break;
+            }
+        }
+        if (!FLAG) {
+            break;
+        }     
+    }
+    return nums;
+}
