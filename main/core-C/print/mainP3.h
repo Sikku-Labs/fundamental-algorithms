ae4e8f42283def577845344ecfeace49aebf3d52
Siddy Thirsty <168836732+playerofiiith@users.noreply.github.com>
2024-07-12 09:44:25 +0530

    Update mainP3.h

--- main/core-C/print/mainP3.h
+++ main/core-C/print/mainP3.h
@@ -4,3 +4,15 @@ sidb95
 bhatoresiddharth@gmail.com 
 12 July 2024
 */
+
+#include "print.h"
+
+void mainP3(char* s) {
+    size1 = lenString(s);
+    int i;
+    for (i = 0; i < size1; i += 1) {
+        printCharacter(s[i]);
+    }
+    printCharacter('\n');
+    return;
+}
