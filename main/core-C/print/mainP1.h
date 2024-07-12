7e4e086cfbf99ec0c7ed65aae061b9afbdfce756
Siddy Thirsty <168836732+playerofiiith@users.noreply.github.com>
2024-07-12 09:30:45 +0530

    Update print

---
+++ main/core-C/print/mainP1.h
@@ -0,0 +1,29 @@
+/*
+Main File Print 1
+sidb95
+bhatoresiddharth@gmail.com 
+12 July 2024
+*/
+
+#include "print.h"
+
+void mainP1Aux(char* s, int size1, int i) {
+    if (i == size1) {
+        return;
+    }
+    else {
+        printCharacter(s[i]);
+    }
+    printCharacter('\n');
+    mainP1Aux(s, size1, i + 1);
+    return;
+}
+
+void mainP1(char* s) {
+    int size1 = 0;
+    while(s[size1] != '\0') {
+        size += 1;
+    }
+    mainP1Aux(s, size1, 0);
+    return;
+}
