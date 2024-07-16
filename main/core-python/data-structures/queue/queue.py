f214d7db3559a49b1b69e5e3434d3477e9fceb2d
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-16 14:31:20 +0530

    Update data-structures

---
+++ main/core-python/data-structures/queue/queue.py
@@ -0,0 +1,31 @@
+"""
+queue.py
+sidb95
+bhatoresiddharth@gmail.com 
+16 July 2024
+"""
+
+class Queue1:
+  def __init__(self):
+    self.Q = []
+  
+  def insert(self, node):
+    L = []
+    L.append(node)
+    for q in self.Q:
+      L.append(q)
+    self.Q = L
+  
+  def remove(self):
+    self.Q.pop()
+  
+  def front(self):
+    return self.Q[0]
+
+  def back(self):
+    size1 = len(self.Q)
+    return self.Q[size1 - 1]
+
+  def empty(self):
+    return (self.Q == [])
+
