fccc3f0cea34ca6232815fd4852afa9e7d8ff516
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-16 13:38:09 +0530

    Update print.py

--- main/core-python/print/print.py
+++ main/core-python/print/print.py
@@ -5,3 +5,11 @@ class Print:
   def printString(self, s: str) -> None:
     print(s)
     return None
+
+  def print character(self, c: char) -> None:
+    print(c, end = "")
+    return None
+
+  def lenString(self, s: str) -> int:
+    return len(s)
+
