2387e42fbcbd38dce0388e368982feb400865551
Siddy Thirsty <168836732+playerofiiith@users.noreply.github.com>
2024-08-11 06:04:39 +0530

    Update stack.py

--- main/core-python/data-structures/stack/stack.py
+++ main/core-python/data-structures/stack/stack.py
@@ -4,3 +4,29 @@ sidb95
 bhatoresiddharth@gmail.com 
 11 August 2024
 """
+
+class Stack:
+    def __init__(self):
+        self.nums = []
+
+    def push(self, val):
+        self.nums.append(val)
+
+    def pop(self):
+        n = self.length()
+        num1 = -1
+        if (not self.empty()):
+            num1 = self.nums[n - 1]
+            self.nums.pop(n - 1)
+        return num1
+
+    def top(self):
+        if (not self.empty()):
+            return self.nums[-1]
+        return -1
+
+    def empty(self):
+        return (self.nums == [])
+
+    def length(self):
+        return len(self.nums)
